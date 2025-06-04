3bf6af1ae2d5b9f8f639e0c2dec19d17d61d37a0
thanakhanp <56973514+thanakhanp@users.noreply.github.com>
2025-06-04 22:20:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #main.py
+'''This code is for running discord bot for save photo on construction line check 
+  1.create folder YYMMDD for each day the photo was sent
+  2.rename photo to YYmmddhhmm_caption and save for the folder in that day'''
+
+######################################################################################################################
 import discord
 import os
 import requests
@@ -6,15 +11,20 @@ from flask import Flask
 from threading import Thread
 from datetime import datetime
 import pytz
+from utils import is_image
 
+
+### for get token and webhook from pipedream ###
 TOKEN = os.environ.get("TOKEN")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
+### Listen for message in discord ###
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-app = Flask('')
 
+### Set up flask for web server ###
+app = Flask('')
 @app.route('/')
 def home():
     return "🤖 Discord bot is alive!"
@@ -34,7 +44,7 @@ async def on_message(message):
     print(f"[LOG] New message from {message.author}: {message.content}")
 
     if message.content.strip() == "!status":
-        await message.channel.send(f"✅ I’m alive as {client.user}")
+        await message.channel.send(f"✅ I’m alive as {client.user}\n Please send me a photo or file to save it.")
         return
 
     if message.author.bot:
@@ -44,26 +54,95 @@ async def on_message(message):
     # ⏰ Restrict to 08:00–22:00 Thai time
     tz = pytz.timezone("Asia/Bangkok")
     now = datetime.now(tz)
-    if now.hour < 7 or now.hour >= 21:
+    if now.hour < 8 or now.hour >= 22:
         print("[LOG] Outside active hours. Ignoring message.")
         return
 
-    if message.attachments:
-        print(f"[LOG] {len(message.attachments)} attachment(s) found")
+    ### Protect no attachement	###
+    if not message.attachments:
+        print("[LOG] Message received without attachment. Skipping.")
+        return
 
-        for attachment in message.attachments:
-            print(f"[LOG] Sending file: {attachment.filename}")
+    print(f"[LOG] {len(message.attachments)} attachment(s) found")
+    caption = message.content.strip().replace("/", "_") or "No_Caption"
+    timestamp = now.strftime("%y%m%d_%H%M")
+    folder_name = now.strftime("%Y%m%d")
+    attachments = message.attachments
+
+    ### If 1 filename	###
+    if len(attachments) == 1:
+        attachment = attachments[0]
+        ### Check file type  ###
+        if is_image(attachment):
+            file_type = "photo"
+            filename = f"{timestamp}_{caption}"
+        else:
+            file_type = "file"
+            filename = f"{timestamp}_{attachment.filename}_{caption}" 
+
+       
+        print(f"[LOG] Sending file: {filename}")
+
+        data = {
+            "filename": attachment.filename,
+            "url": attachment.url,
+            "author": str(message.author),
+            "channel": str(message.channel),
+            "folder": folder_name,
+            "renamed": filename,
+            "caption": caption,
+            "ftype": file_type
+        }
+
+        try:
+            res = requests.post(WEBHOOK_URL, json=data)
+            print(f"[LOG] Sent to Pipedream. Response: {res.status_code}")
+            if res.status_code == 200 or 202:
+                await message.channel.send(f"✅ File `{filename}` was already uploaded")
+            else:
+                await message.channel.send(f"❌ Failed to upload `{filename}`")
+        except Exception as e:
+            print(f"[ERROR] Failed to send to Pipedream: {e}")
+            await message.channel.send(f"❌ Upload error: {e}")
+
+    ### Else More than 1 files	###
+    else:
+        for i, attachment in enumerate(attachments, start=1):
+            ### Check file type  ###
+            if is_image(attachment):
+                file_type = "photo"
+                indexed_caption = f"{caption}_{i}"
+                filename = f"{timestamp}_{caption}_{i}"
+                caption_to_use = indexed_caption
+            else:
+                file_type = "file"
+                filename = f"{timestamp}_{attachment.filename}"
+                caption_to_use = caption 
+            
+
+            print(f"[LOG] Sending file: {filename}")
+
             data = {
                 "filename": attachment.filename,
                 "url": attachment.url,
                 "author": str(message.author),
-                "channel": str(message.channel)
+                "channel": str(message.channel),
+                "folder": folder_name,
+                "renamed": filename,
+                "caption": caption_to_use,
+                "ftype": file_type
             }
+
             try:
                 res = requests.post(WEBHOOK_URL, json=data)
                 print(f"[LOG] Sent to Pipedream. Response: {res.status_code}")
-                await message.channel.send(f"✅ Data uploaded")
+                if res.status_code == 200 or 202 :
+                    await message.channel.send(f"✅ File `{filename}` was already uploaded")
+                else:
+                    await message.channel.send(f"❌ Failed to upload `{filename}`")
             except Exception as e:
                 print(f"[ERROR] Failed to send to Pipedream: {e}")
+                await message.channel.send(f"❌ Upload error: {e}")
 
 client.run(TOKEN)
+
